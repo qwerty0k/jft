8664a88baeab9b96496a05fb85b62b65d4b77c11
qwerty0k <artem.sergeev99@mail.ru>
2021-11-25 09:38:18 +0300

    First autotest

--- sandbox/src/main/java/ru/stqa/jft/sandbox/Main.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/Main.java
@@ -3,23 +3,23 @@ package ru.stqa.jft.sandbox;
 public class Main {
 
     public static void main(String[] args) {
+
         hello("Java!");
-        hello("Artem");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+        Rectangle r = new Rectangle(5,6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p1 = new Point(5, 5);
+        Point p2 = new Point(6,6);
+        System.out.println("Расстояние между двумя точками с координатами: " + p1.a + "," + p1.b + " и " + p2.a + "," + p2.b + " = " + p1.distance(p1,p2));
+
     }
+
     public static void hello(String somebody) {
         System.out.println("Hi " + somebody);
     }
-    public static double area(double len) {
-        return len * len;
-    }
-    public static double area(double a, double b) {
-        return a * b;
-    }
+
 }
---
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.jft.sandbox;
+
+public class Point {
+
+    public double a;
+    public double b;
+
+    public Point(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.b-p1.b),2)+Math.pow((p2.a-p1.a),2));
+    }
+}
