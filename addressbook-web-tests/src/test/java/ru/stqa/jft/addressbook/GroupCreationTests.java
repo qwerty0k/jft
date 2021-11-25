df920f2ebb6daccec7110dcc56bef3c97ed6e580
qwerty0k <artem.sergeev99@mail.ru>
2021-11-25 12:05:46 +0300

    Created TestBase and new Test for deletion

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/GroupCreationTests.java
@@ -1,42 +1,16 @@
 package ru.stqa.jft.addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class GroupCreationTests {
-  FirefoxDriver driver;
-
-  @BeforeMethod
-  public void setUp() {
-    System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox esr\\firefox.exe");
-    System.setProperty("webdriver.gecko.driver","C:\\Program Files\\Mozilla Firefox esr\\geckodriver.exe");
-    driver = new FirefoxDriver();
-    driver.get("http://localhost/addressbook/");
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-  }
-  @AfterMethod
-  public void tearDown() {
-    driver.quit();
-  }
+public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("Test1");
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).sendKeys("Test2");
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).sendKeys("Test3");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("group page")).click();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
+    submitGroupCreation();
+    returnGroupPage();
   }
+
 }
