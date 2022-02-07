9996ca612d8e52f055c8ab0d0b1eeb668d461d03
qwerty0k <artem.sergeev99@mail.ru>
2022-02-07 10:18:53 +0300

    Cache
    added cache

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/GroupDeletionTests.java
@@ -1,30 +1,34 @@
 package ru.stqa.jft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.jft.addressbook.model.GroupData;
-import java.util.List;
+import ru.stqa.jft.addressbook.model.Groups;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.testng.Assert.*;
 
 public class GroupDeletionTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().groupPage();
-    if (app.group().list().size() == 0) {
+    if (app.group().all().size() == 0) {
       app.group().create(new GroupData().withName("Test1"));
     }
   }
   @Test
   public void testGroupDeletion() {
-    List<GroupData> before = app.group().list();
-    int index = before.size() - 1;
-    app.group().delete(index);
-    List<GroupData> after = app.group().list();
-    Assert.assertEquals(after.size(), before.size() - 1);
+    Groups before = app.group().all();
+    GroupData deletedGroup = before.iterator().next();
+    app.group().delete(deletedGroup);
+    Groups after = app.group().all();
+    assertEquals(after.size(), before.size() - 1);
 
-    before.remove(index);
-    Assert.assertEquals(before, after);
+    assertThat(after, equalTo(before.without(deletedGroup)));
 
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/GroupModificationTest.java
@@ -1,37 +1,39 @@
 package ru.stqa.jft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.jft.addressbook.model.GroupData;
-import java.util.Comparator;
-import java.util.List;
+import ru.stqa.jft.addressbook.model.Groups;
+
+import static org.hamcrest.CoreMatchers.*;
 
 public class GroupModificationTest extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
         app.goTo().groupPage();
-        if (app.group().list().size() == 0) {
+        if (app.group().all().size() == 0) {
             app.group().create(new GroupData().withName("Test1"));
         }
     }
 
     @Test
     public void testGroupModification() {
-        List<GroupData> before = app.group().list();
+        Groups before = app.group().all();
+        GroupData modifyGroup = before.iterator().next();
         int index = before.size() - 1;
-        GroupData group = new GroupData().withId(before.get(index).getId()).withName("editedTest1").withHeader("editedTest2").withFooter("editedTest3");
-        app.group().modify(index, group);
-        List<GroupData> after = app.group().list();
+        GroupData group = new GroupData().withId(modifyGroup.getId()).withName("editedTest1").withHeader("editedTest2").withFooter("editedTest3");
+        app.group().modify(group);
+        Groups after = app.group().all();
         Assert.assertEquals(after.size(), before.size());
 
-        before.remove(index);
-        before.add(group);
-        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        /*Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
-        after.sort(byId);
-        Assert.assertEquals(before, after);
+        after.sort(byId);*/
+        MatcherAssert.assertThat(after, equalTo(before.without(modifyGroup).withAdded(group)));
     }
 
 
